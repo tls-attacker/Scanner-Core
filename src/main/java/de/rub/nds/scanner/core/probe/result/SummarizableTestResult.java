e03362b53c6857eb8b039b358049ac842125b564
Sven Niclas Hebrok <sven.hebrok@upb.de>
2023-11-13 15:56:40 +0100

    Incorporate feedback form PR

---
+++ src/main/java/de/rub/nds/scanner/core/probe/result/SummarizableTestResult.java
@@ -0,0 +1,36 @@
+/*
+ * Scanner Core - A modular framework for probe definition, execution, and result analysis.
+ *
+ * Copyright 2017-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.scanner.core.probe.result;
+
+/**
+ * A complex test result that can still be summarized into a single TestResults. This summary might
+ * be generated on the fly (from the contained details), or set explicitly (e.g. in case of an
+ * error)
+ */
+public interface SummarizableTestResult extends TestResult {
+    TestResults getSummarizedResult();
+
+    /**
+     * @return Whether the summary was explicitly set instead of generated on the fly.
+     */
+    boolean isExplicitSummary();
+
+    @Override
+    default boolean equalsExpectedResult(TestResult other) {
+        if (other instanceof TestResults) {
+            return getSummarizedResult().equals(other);
+        }
+        return this.equals(other);
+    }
+
+    @Override
+    default String getName() {
+        return getSummarizedResult().getName();
+    }
+}
