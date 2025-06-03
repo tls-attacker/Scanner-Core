0fde9ee1809371d8b32e9d32d43bc8c7fa4361a4
Robert Merget <robert.merget@tii.ae>
2025-06-04 01:14:34 +0400

    removed code using guideline check result as non-abstract

---
+++ src/main/java/de/rub/nds/scanner/core/guideline/FailedCheckGuidelineResult.java
@@ -0,0 +1,20 @@
+/*
+ * Scanner Core - A Modular Framework for Probe Definition, Execution, and Result Analysis.
+ *
+ * Copyright 2017-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.scanner.core.guideline;
+
+public class FailedCheckGuidelineResult extends GuidelineCheckResult {
+
+    public FailedCheckGuidelineResult(String checkName, GuidelineAdherence adherence) {
+        super(checkName, adherence);
+    }
+
+    public FailedCheckGuidelineResult(String checkName, GuidelineAdherence adherence, String hint) {
+        super(checkName, adherence, hint);
+    }
+}
