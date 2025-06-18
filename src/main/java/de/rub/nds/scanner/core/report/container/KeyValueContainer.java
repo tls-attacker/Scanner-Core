8679d0ba2b4b7961a725bcc1544050e9c250eb54
Robert Merget <robert.merget@tii.ae>
2025-06-18 21:00:16 +0000

    Add Javadoc to KeyValueContainer

--- src/main/java/de/rub/nds/scanner/core/report/container/KeyValueContainer.java
+++ src/main/java/de/rub/nds/scanner/core/report/container/KeyValueContainer.java
@@ -13,6 +13,10 @@ import de.rub.nds.scanner.core.report.AnsiColor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Container for displaying key-value pairs in scanner reports.
+ * Provides formatted output with configurable colors for both keys and values.
+ */
 public class KeyValueContainer extends ReportContainer {
 
     private static final Logger LOGGER = LogManager.getLogger();
@@ -25,6 +29,14 @@ public class KeyValueContainer extends ReportContainer {
     private String value;
     private AnsiColor valueColor;
 
+    /**
+     * Creates a new KeyValueContainer with specified colors.
+     * 
+     * @param key The key text to display
+     * @param keyColor The color for the key
+     * @param value The value text to display
+     * @param valueColor The color for the value
+     */
     public KeyValueContainer(String key, AnsiColor keyColor, String value, AnsiColor valueColor) {
         super(ScannerDetail.NORMAL);
         this.key = key;
@@ -33,6 +45,14 @@ public class KeyValueContainer extends ReportContainer {
         this.valueColor = valueColor;
     }
 
+    /**
+     * Prints the key-value pair to the provided StringBuilder.
+     * The key is padded to ensure consistent alignment.
+     * 
+     * @param builder The StringBuilder to append the output to
+     * @param depth The indentation depth level
+     * @param useColor Whether to use ANSI color codes in the output
+     */
     @Override
     public void print(StringBuilder builder, int depth, boolean useColor) {
         addDepth(builder, depth);
@@ -58,34 +78,74 @@ public class KeyValueContainer extends ReportContainer {
         }
     }
 
+    /**
+     * Gets the key text.
+     * 
+     * @return The key text
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * Sets the key text.
+     * 
+     * @param key The new key text
+     */
     public void setKey(String key) {
         this.key = key;
     }
 
+    /**
+     * Gets the key color.
+     * 
+     * @return The ANSI color for the key
+     */
     public AnsiColor getKeyColor() {
         return keyColor;
     }
 
+    /**
+     * Sets the key color.
+     * 
+     * @param keyColor The new ANSI color for the key
+     */
     public void setKeyColor(AnsiColor keyColor) {
         this.keyColor = keyColor;
     }
 
+    /**
+     * Gets the value text.
+     * 
+     * @return The value text
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Sets the value text.
+     * 
+     * @param value The new value text
+     */
     public void setValue(String value) {
         this.value = value;
     }
 
+    /**
+     * Gets the value color.
+     * 
+     * @return The ANSI color for the value
+     */
     public AnsiColor getValueColor() {
         return valueColor;
     }
 
+    /**
+     * Sets the value color.
+     * 
+     * @param valueColor The new ANSI color for the value
+     */
     public void setValueColor(AnsiColor valueColor) {
         this.valueColor = valueColor;
     }
