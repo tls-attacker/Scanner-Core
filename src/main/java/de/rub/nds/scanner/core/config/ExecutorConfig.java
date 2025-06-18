c368686472a06144eb167af63470e515f749178e
Robert Merget <robert.merget@tii.ae>
2025-06-18 20:38:56 +0000

    Add Javadoc to ExecutorConfig public methods
    Added documentation for all public methods including:
    - Getter/setter methods for excluded probes, scan details, and report settings
    - Methods for managing probe lists and adding probes
    - Configuration methods for threading and timeouts
    - Output file and multithreading status checks

--- src/main/java/de/rub/nds/scanner/core/config/ExecutorConfig.java
+++ src/main/java/de/rub/nds/scanner/core/config/ExecutorConfig.java
@@ -66,58 +66,128 @@ public final class ExecutorConfig {
         // Default constructor
     }
 
+    /**
+     * Returns a copy of the list of probe types that are excluded from scanning.
+     *
+     * @return a new list containing the excluded probe types
+     */
     public List<ProbeType> getExcludedProbes() {
         return new LinkedList<>(excludedProbes);
     }
 
+    /**
+     * Sets the list of probe types to be excluded from scanning.
+     *
+     * @param excludedProbes the list of probe types to exclude
+     */
     public void setExcludedProbes(List<ProbeType> excludedProbes) {
         this.excludedProbes = new LinkedList<>(excludedProbes);
     }
 
+    /**
+     * Returns the scanner detail level for the scan operation.
+     *
+     * @return the current scanner detail level
+     */
     public ScannerDetail getScanDetail() {
         return scanDetail;
     }
 
+    /**
+     * Sets the scanner detail level for the scan operation.
+     *
+     * @param scanDetail the scanner detail level to set
+     */
     public void setScanDetail(ScannerDetail scanDetail) {
         this.scanDetail = scanDetail;
     }
 
+    /**
+     * Returns the scanner detail level for post-analysis operations.
+     *
+     * @return the current post-analysis detail level
+     */
     public ScannerDetail getPostAnalysisDetail() {
         return postAnalysisDetail;
     }
 
+    /**
+     * Sets the scanner detail level for post-analysis operations.
+     *
+     * @param postAnalysisDetail the post-analysis detail level to set
+     */
     public void setPostAnalysisDetail(ScannerDetail postAnalysisDetail) {
         this.postAnalysisDetail = postAnalysisDetail;
     }
 
+    /**
+     * Returns the scanner detail level for report generation.
+     *
+     * @return the current report detail level
+     */
     public ScannerDetail getReportDetail() {
         return reportDetail;
     }
 
+    /**
+     * Sets the scanner detail level for report generation.
+     *
+     * @param reportDetail the report detail level to set
+     */
     public void setReportDetail(ScannerDetail reportDetail) {
         this.reportDetail = reportDetail;
     }
 
+    /**
+     * Checks if colored text output is disabled.
+     *
+     * @return true if colored text is disabled, false otherwise
+     */
     public boolean isNoColor() {
         return noColor;
     }
 
+    /**
+     * Sets whether colored text output should be disabled.
+     *
+     * @param noColor true to disable colored text, false to enable it
+     */
     public void setNoColor(boolean noColor) {
         this.noColor = noColor;
     }
 
+    /**
+     * Returns a copy of the list of probe types to be executed.
+     *
+     * @return a new list containing the probe types, or null if not set
+     */
     public List<ProbeType> getProbes() {
         return probes == null ? null : new LinkedList<>(probes);
     }
 
+    /**
+     * Sets the list of probe types to be executed.
+     *
+     * @param probes the list of probe types to execute, or null to clear
+     */
     public void setProbes(List<ProbeType> probes) {
         this.probes = probes == null ? null : new LinkedList<>(probes);
     }
 
+    /**
+     * Sets the probe types to be executed using a varargs parameter.
+     *
+     * @param probes the probe types to execute
+     */
     public void setProbes(ProbeType... probes) {
         this.probes = Arrays.asList(probes);
     }
 
+    /**
+     * Adds additional probe types to the existing list of probes to be executed.
+     *
+     * @param probes the list of probe types to add
+     */
     public void addProbes(List<ProbeType> probes) {
         if (this.probes == null) {
             this.probes = new LinkedList<>();
@@ -125,6 +195,11 @@ public final class ExecutorConfig {
         this.probes.addAll(probes);
     }
 
+    /**
+     * Adds additional probe types to the existing list using a varargs parameter.
+     *
+     * @param probes the probe types to add
+     */
     public void addProbes(ProbeType... probes) {
         if (this.probes == null) {
             this.probes = new LinkedList<>();
@@ -132,42 +207,92 @@ public final class ExecutorConfig {
         this.probes.addAll(Arrays.asList(probes));
     }
 
+    /**
+     * Returns the timeout value for each probe execution in milliseconds.
+     *
+     * @return the probe timeout in milliseconds
+     */
     public int getProbeTimeout() {
         return probeTimeout;
     }
 
+    /**
+     * Sets the timeout value for each probe execution in milliseconds.
+     *
+     * @param probeTimeout the probe timeout in milliseconds
+     */
     public void setProbeTimeout(int probeTimeout) {
         this.probeTimeout = probeTimeout;
     }
 
+    /**
+     * Checks if the report should be written to a file.
+     *
+     * @return true if an output file is specified, false otherwise
+     */
     public boolean isWriteReportToFile() {
         return outputFile != null;
     }
 
+    /**
+     * Returns the path to the output file for the report.
+     *
+     * @return the output file path, or null if not specified
+     */
     public String getOutputFile() {
         return outputFile;
     }
 
+    /**
+     * Sets the path to the output file for the report.
+     *
+     * @param outputFile the output file path
+     */
     public void setOutputFile(String outputFile) {
         this.outputFile = outputFile;
     }
 
+    /**
+     * Returns the number of threads used for executing different probes in parallel.
+     *
+     * @return the number of parallel probe threads
+     */
     public int getParallelProbes() {
         return parallelProbes;
     }
 
+    /**
+     * Sets the number of threads used for executing different probes in parallel.
+     *
+     * @param parallelProbes the number of parallel probe threads
+     */
     public void setParallelProbes(int parallelProbes) {
         this.parallelProbes = parallelProbes;
     }
 
+    /**
+     * Returns the maximum number of threads used to execute probes.
+     *
+     * @return the maximum number of overall threads
+     */
     public int getOverallThreads() {
         return overallThreads;
     }
 
+    /**
+     * Sets the maximum number of threads used to execute probes.
+     *
+     * @param overallThreads the maximum number of overall threads
+     */
     public void setOverallThreads(int overallThreads) {
         this.overallThreads = overallThreads;
     }
 
+    /**
+     * Checks if the scanner is configured to run in multithreaded mode.
+     *
+     * @return true if either parallel probes or overall threads is greater than 1
+     */
     public boolean isMultithreaded() {
         return parallelProbes > 1 || overallThreads > 1;
     }
